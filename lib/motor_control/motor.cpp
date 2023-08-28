a14c2119d51823000f7da5928d8789c4a6be491d
sabanekko3 <tetayaae@gmail.com>
2023-08-28 23:44:52 +0900

    write driver/motor class

---
+++ lib/motor_control/motor.cpp
@@ -0,0 +1,48 @@
+#include "motor.hpp"
+
+/////////////////////////////////////////////////
+//driver class///////////////////////////////////
+/////////////////////////////////////////////////  
+void DRIVER::init(void){
+    pinMode((int)pin.MODE, OUTPUT);
+    pinMode((int)pin.STBY, OUTPUT);
+    pinMode((int)pin.ERR, INPUT);
+
+    //start phase mode
+    digitalWrite((int)pin.MODE,LOW);
+
+    digitalWrite((int)pin.STBY,HIGH);
+    delay(1);
+    digitalWrite((int)pin.MODE,HIGH);
+    delay(1);
+}
+
+void DRIVER::reset(void){
+    digitalWrite((int)pin.STBY,LOW);
+    delay(1);
+
+    init();
+}
+
+/////////////////////////////////////////////////
+//motor class////////////////////////////////////
+/////////////////////////////////////////////////
+void MOTOR::init(void){
+    ledcSetup(pin.pwm.ch,PWM_FRQ,PWM_WIDTH); 
+    ledcAttachPin((int)pin.pwm.pin, pin.pwm.ch);
+
+    pinMode((int)pin.en,OUTPUT);
+    digitalWrite((int)pin.en,HIGH);
+}
+
+void MOTOR::out(float duty){
+    assert(duty<1.0f || duty>-1.0f);
+
+    digitalWrite((int)pin.en,HIGH);
+    ledcWrite(pin.pwm.ch,(duty+1)*PWM_RESOL*motor_k);
+}
+
+void MOTOR::free(void){
+    digitalWrite((int)pin.en,LOW);
+    ledcWrite(pin.pwm.ch,0);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,91 +1,30 @@
 #include <Arduino.h>
 #include <assert.h>
 
-typedef struct{
-  uint8_t pin;
-  uint8_t ch;
-}pwm_t;
+#include "board_pins.hpp"
+#include "motor.hpp"
 
-typedef struct{
-  pwm_t pwm1;
-  pwm_t pwm2;
-}mpwm_t;
+DRIVER md1(DRIVER1_pin);
 
-typedef struct{
-  mpwm_t motor1;
-  mpwm_t motor2;
-  uint8_t ERR;
-  uint8_t MODE;
-  uint8_t STBY;
-} md_pin_t;
-
-const pwm_t pwm_a1 = {32,0};
-const pwm_t pwm_a2 = {25,1};
-const pwm_t pwm_b1 = {33,2};
-const pwm_t pwm_b2 = {26,3};
-
-const mpwm_t mpwm1 = {pwm_a1,pwm_a2};
-const mpwm_t mpwm2 = {pwm_b1,pwm_b2};
-const md_pin_t md1 = {mpwm1,mpwm2,27,14,12};
-
-enum class MD_MODE{
-  IN,
-  PHASE,
-};
-
-void MD_init(md_pin_t md,MD_MODE mode);
-void motor_turn(mpwm_t pin,float duty);
+MOTOR motor1(MOTOR1_pin,1.0f);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.printf("start\r\n");
-  MD_init(md1,MD_MODE::PHASE);
+
+  md1.init();
+  motor1.init();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  motor_turn(mpwm1,1);
+  motor1.out(1);
   delay(1000);
-  motor_turn(mpwm1,0);
+  motor1.out(0);
   delay(1000);
-  motor_turn(mpwm1,-1);
+  motor1.out(-1); 
   delay(1000);
-  motor_turn(mpwm1,0);
+  motor1.out(0);
   delay(1000);
-}
-
-void MD_init(md_pin_t md,MD_MODE mode){
-  //GPIO init
-  pinMode(md.MODE, OUTPUT);
-  pinMode(md.STBY, OUTPUT);
-  pinMode(md.ERR, INPUT);
-
-  //PWM init
-  ledcSetup(md.motor1.pwm1.ch,78125,10); 
-  ledcAttachPin(md.motor1.pwm1.pin, md.motor1.pwm1.ch);
-  ledcSetup(md.motor1.pwm2.ch,78125,10); 
-  ledcAttachPin(md.motor1.pwm2.pin,md.motor1.pwm2.ch);
-
-  ledcSetup(md.motor2.pwm1.ch,78125,10); 
-  ledcAttachPin(md.motor2.pwm1.pin, md.motor2.pwm1.ch);
-  ledcSetup(md.motor2.pwm2.ch,78125,10); 
-  ledcAttachPin(md.motor2.pwm2.pin,md.motor2.pwm2.ch);
-
-  //set
-  switch(mode){
-    case MD_MODE::IN:
-      digitalWrite(md.MODE,LOW);
-      break;
-    case MD_MODE::PHASE:
-      digitalWrite(md.MODE,HIGH);
-      break;
-  }
-  digitalWrite(md.STBY,HIGH);
-}
-
-void motor_turn(mpwm_t pin,float duty){
-  assert(duty<1 || duty>-1);
-  ledcWrite(pin.pwm2.ch,(duty+1)*512);
-  ledcWrite(pin.pwm1.ch,1024);
 }
